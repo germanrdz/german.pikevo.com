2b552a9a7c788cfee86e8707c64d61941d3f5cf1
GerManson <german@phisoft.com.mx>
2011-10-31 12:37:09 -0700

    hovers style added to the controls menu

--- index.php
+++ index.php
@@ -16,9 +16,13 @@
             
             <h1>My Picture Evolution</h1>
             <p>
-                I set a timer on my laptop, on my desktop machine and on my phone, to remind me every day to take a picture of myself at 3:00 pm, no matter what i am doing or where i am.
+                I've set a timer on my laptop, on my desktop machine and on my phone, to remind me every day to take a picture of myself at 3:00 pm, no matter what i am doing or where i am.
             </p>
             
+            <p>
+                I've coded & configured a system that allows me to upload this pictures and automatically create a slideshow of them with the hope of seing interesting facts about me changing, maybe the sadness in my eyes, or the happiness of my smile, the getting fatter or slimmer, how bad i look when hungover, etc.
+            </p>
+
             <p>
                 This is part of a little experiment i am running, if you came here by accident, move on and ignore this page.
             </p>
@@ -45,7 +49,6 @@
 
             <div id="timeline">
             </div>
-
 
         </div> <!-- content -->
 
