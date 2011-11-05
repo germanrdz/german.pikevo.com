bc274a316cfdf8d0c91f07fa8f65b9561167848b
GerManson <german@phisoft.com.mx>
2011-11-04 17:16:32 -0700

    file sorting withing PHP

--- pikevo.core.php
+++ pikevo.core.php
@@ -1,35 +1,36 @@
 <?php
 
 
-	function Action($option, $param) {		
-		switch($option) {
-			case "getImagesList":
-				getImagesList($param);
-				break;
-		}		
-	}
+function Action($option, $param) {		
+    switch($option) {
+    case "getImagesList":
+        getImagesList($param);
+        break;
+    }		
+}
 		
-	function getImagesList($folder) {
+function getImagesList($folder) {
 		
-		$fileList = array();
-		$count = 0;
+    $fileList = array();
+    $count = 0;
 		
-		if ($handle = opendir($folder))
-		{
-		    while (false !== ($file = readdir($handle)))
-			{
-		        if ($file != "." && $file != "..")
-				{
-		          	// adding file to the filelist array
-		  			$fileList[$count] = $file;
-					$count++;
-		        }
-		    }
-		    closedir($handle);
-		}
+    if ($handle = opendir($folder)) {
+        while (false !== ($file = readdir($handle)))
+            {
+                if ($file != "." && $file != "..")
+                    {
+                        // adding file to the filelist array
+                        $fileList[$count] = $file;
+                        $count++;
+                    }
+            }
+        closedir($handle);
+    }
+    
+    sort($fileList);
 		
-		// return json object
-		echo json_encode($fileList); 		
-	}
+    // return json object
+    echo json_encode($fileList); 		
+}
 
 ?>
